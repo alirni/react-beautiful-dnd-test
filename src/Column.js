88b9c3e3de77ca893845f8ba30a0c588423bfc39
AliRni <aliramezani19.1@gmail.com>
2019-06-19 16:29:18 +0430

    fix

--- src/Column.js
+++ src/Column.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core';
+import Task from "./Task";
 
-export default class Column extends React.Component {
+const styles = {
+  container: {
+    border: '1px solid red',
+    margin: 10,
+  },
+  taskList: {
+    border: '1px solid blue',
+    margin: 10,
+    padding: 10,
+  }
+};
+
+class Column extends React.Component {
   render() {
-    return this.props.column.title;
+    const {classes, column, tasks} = this.props;
+
+    return (
+      <div className={classes.container}>
+        <div className={classes.title}>
+          {column.title}
+        </div>
+        <div className={classes.taskList}>
+          {tasks.map( task => <Task key={task.id} task={task} />)}
+        </div>
+      </div>
+    );
   }
 }
 
+export default withStyles(styles, { withTheme: true })(Column);
