fe9e196754d3c40a803b61ee710d67da7b2ba457
AliRni <aliramezani19.1@gmail.com>
2019-06-19 15:57:40 +0430

    create column

--- src/Column.js
+++ src/Column.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import initialData from './initial-data';
-import Column from './Column';
 
-class App extends React.Component {
-  state = initialData;
+export default class Column extends React.Component {
   render() {
-    return  this.state.columnOrder.map( columnId => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map( taskId => this.state.tasks[taskId]);
-      return <Column key={column.id} column={column} tasks={tasks} />;
-    })
+    return this.props.column.title;
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
